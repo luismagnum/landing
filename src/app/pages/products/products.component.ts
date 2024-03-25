d6d534cbe0b60259b8420d519a009b6ae9e92549
luismagnum <luismagnum1@gmail.com>
2024-03-25 15:43:29 -0300

    api

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { IProduct } from '../../models/product.model';
+import { Router } from 'express';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
-
+export class ProductsComponent implements OnInit {
+  
+  productList: IProduct[]= []
+  private _apiService = inject(ApiService);
+  
+  
+  ngOnInit(): void {
+    this._apiService.getProducts().subscribe((data:IProduct[]) =>{
+      this.productList = data;
+    })
+  }
 }
